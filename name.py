f4c7aadd6b070c0d37ce680351170fddda4423b4
Elbichosxmessi <147065587+Elbichosxmessi@users.noreply.github.com>
2023-12-12 09:15:39 -0500

    Add files via upload

---
+++ name.py
@@ -0,0 +1,54 @@
+#EJERCICIO_1
+
+#Suma_de_dos_numeros_int
+a=34
+b=567
+c=(a+b)
+print(c)
+
+#Resta_de_dos_numeros_de_punto_float
+a=2.5
+b=4.23
+d=(a-b)
+print(d)
+
+#Muliplicacion_de_numeros_int
+a=56
+b=3
+e=(a*b)
+print(e)
+
+#Divicion_de_dos_numeros_de_float
+a=4.4
+b=2.1
+z=(a/b)
+print(z)
+
+#EJERCICIO_2
+
+Nombre_completo = "Edisson_Sebastian_Chacon_Vaquez"
+len(Nombre_completo)
+print(len(Nombre_completo))
+print((Nombre_completo),len(Nombre_completo))
+#Este_programa_tiene_la_funcion_de_imprimir_un_nombre_completo_y_su_longitud_utilizando_los_comandos
+#basicos_de_programacion
+
+#EJERCICIO_3
+
+# Paso 1: Declara una variable entera y otra de punto flotante
+a = 5
+f = 3.14
+#Cambio_de_variable_tipo_int_a_float
+print(a)
+print(float(a))
+#Cambio_de_variable_tipo_float_a_int
+print(f)
+print(int(f))
+
+#EJERCICIO_4
+#Ejercicio_1:_Se_realizo_un_programa_que_sume_reste_multiplique_y_divida_numero_int_float
+#Ejercicio_2:_Nombramos_una_variable_de_tipo_string_para_designar_un_nombre_y_su_longitud
+#Ejercicio_3:_Cambio_de_variables_int_y_float_a_float_e_int
+
+
+
